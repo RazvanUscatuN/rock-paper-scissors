1ae5e26bf168e9b4633a9c6bfab3f23c1dfd3dc9
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2023-11-24 14:17:05 +0200

    Add basic ui and logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 //Variable that is used for computer choice
 const gameOptions = ["rock", "paper", "scissors"];
 
+//Global Variable in order to keep score
+let userScore = 0;
+let computerScore = 0;
+
 
 //Create function to get user input with prompt
 function playerSelection() {
@@ -25,69 +29,120 @@ function getComputerChoice(options) {
 
 //Game logic of a round
 function playRound(playerSelection, computerSelection) {
-    let playerPoint = "player";
-    let computerPoint = "computer";
-    let drawPoint = "draw";
-
+    
     if(playerSelection == computerSelection){
         console.log("Draw");
-        return drawPoint;
+        return "draw";
     }
     else if(playerSelection == "rock" && computerSelection == "scissors"){
         console.log("You win! Rock beats scissors. You get this point");
-        return playerPoint;
+        return "player";
     }
     else if(playerSelection == "rock" && computerSelection == "paper"){
         console.log("You lose! Paper beats rock. Computer get this point");
-        return computerPoint;
+        return "computer";
     }
     else if(playerSelection == "paper" && computerSelection == "rock"){
         console.log("You win! Paper beats Rock. You get this point");
-        return playerPoint;
+        return "player";
     }
     else if(playerSelection == "paper" && computerSelection == "scissors"){
         console.log("You lose! Scissors beats Paper. Computer get this point");
-        return computerPoint;
+        return "computer";
     }
     else if(playerSelection == "scissors" && computerSelection == "paper"){
         console.log("You win! Scissors beats paper . You get this point");
-        return playerPoint;
+        return "player";
     }
     else if(playerSelection == "scissors" && computerSelection == "rock"){
         console.log("You lose! Rock beats Scissors. Computer get this point");
-        return computerPoint;
+        return "computer";
     }
 }
 
 
-//Logic of game with 5 round and keeping score and show it
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let player;
-    let computer;
 
-    for(let i=0; i<5; i++){
-        player = playerSelection();
-        computer = getComputerChoice(gameOptions);
-        playPoint = playRound(player,computer);
-
-        if ( playPoint == "player"){
-            playerScore++; 
-        } else if (playPoint == "computer"){
-            computerScore++;            
-        } else {
-            continue;
-        }
-    }
-
-    console.log(`This is player score: ${playerScore}`);
-    console.log(`This is computer score: ${computerScore}`)
-}
+// Function to handle the button click
+function handleButtonClick(playerChoice) {
+    const paraResult = document.querySelector("#winner-para");
+    //make sure that the span is not keep adding the result
+    const oldSpan = paraResult.querySelector("span");
     
 
+    if (oldSpan) {
+        oldSpan.remove();
+    }
 
-game();
+    const paraUserScore = document.querySelector("#user-score-para")
+    let scoreUserSpan = paraUserScore.querySelector("span");
+    if (!scoreUserSpan) {
+        scoreUserSpan = document.createElement("span");
+        paraUserScore.appendChild(scoreUserSpan);
+    }
+
+    const paraComputerScore = document.querySelector("#computer-score-para")
+    let scoreComputerSpan = paraComputerScore.querySelector("span");
+    if (!scoreComputerSpan) {
+        scoreComputerSpan = document.createElement("span");
+        paraComputerScore.appendChild(scoreComputerSpan);
+    }
+
+    const spanText = document.createElement("span");
+    const computerChoice = getComputerChoice(gameOptions);
+    let whoWins = playRound(playerChoice, computerChoice);
+    
+  
+    if(whoWins == "player"){
+        spanText.textContent = "Player Wins a point";
+        paraResult.appendChild(spanText);
+        userScore += 1;
+
+        // Update the score in the existing span element
+        scoreUserSpan.textContent = userScore;
+        
+    }else if(whoWins == "computer"){
+        spanText.textContent = "Computer Wins a point";
+        paraResult.appendChild(spanText);
+        computerScore += 1;
+
+        // Update the score in the existing span element
+        scoreComputerSpan.textContent = computerScore;
+        
+    }else{
+        spanText.textContent = "Draw, no point added";
+        paraResult.appendChild(spanText);
+    }
+    if(userScore == 5){
+        alert("Player Win!")
+        userScore = 0;
+        computerScore = 0;
+        
+    }else if(computerScore == 5){
+        alert("Computer win!")
+        userScore = 0;
+        computerScore = 0;
+    }
+}
+
+
+
+
+
+// Add event listeners to each button using querySelector
+document.querySelector("#btn-rock").addEventListener("click", function() {
+    handleButtonClick("rock");
+});
+
+document.querySelector("#btn-paper").addEventListener("click", function() {
+    handleButtonClick("paper");
+});
+
+document.querySelector("#btn-scissors").addEventListener("click", function() {
+    handleButtonClick("scissors");
+});
+
+
+
 
 
 
